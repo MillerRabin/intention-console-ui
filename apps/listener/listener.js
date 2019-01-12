1565a4f6956ce0d1a9e460f06fa1c9487a1e57c6
MillerRabin <millerrabin@raintech.su>
2019-01-12 15:15:45 +0300

    added speech recognition

--- apps/listener/listener.js
+++ apps/listener/listener.js
@@ -1,57 +1,87 @@
 import loader from '../loader.js';
 
+function createRecognition() {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.lang = 'ru-RU';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 5;
+    recognition.continuous = true;
+    return recognition;
+}
+
+function formatAnswer(results) {
+    const text = event.results[0][0].transcript;
+    return {
+        alternatives: event.results,
+        text: data.newText
+    };
+}
+
+
+function startRecognition(recognition, data) {
+    recognition.onresult = (event) => {
+        console.log(event.results[0]);
+        const answer = formatAnswer(event.results);
+        data.rtext =answer.text;
+        data.answers.push(answer);
+    };
+
+    recognition.onerror = function(event) {
+        console.log('Error occurred in recognition: ' + event.error);
+        if (event.error != 'aborted')
+            setTimeout(() => {
+                startRecognition(recognition, data)
+            }, 0);
+    };
+
+    recognition.onaudioend = function(error) {
+        console.log('Stopped. Restarting');
+        setTimeout(() => {
+            startRecognition(recognition, data)
+        }, 1000);
+    };
+
+    stopRecognition(recognition);
+    try {
+        if (!enableListener) throw new Error('Sound listening is disabled');
+        recognition.start();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function stopRecognition(recognition) {
+    recognition.abort();
+}
+
+let enableListener = false;
+
 loader.application('listener', [async () => {
     function init() {
         return {
             loaded: false,
-            rText: ''
+            newText: '',
+            answers: []
         }
     }
 
     await loader.createVueTemplate({ path: 'apps/listener/listener.html', id: 'Listener-Template' });
     const res = {};
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
-    const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+    const recognition = createRecognition();
 
     res.Constructor = Vue.component('listener', {
         template: '#Listener-Template',
         data: init,
-        watch:{
-            $route (){
-                this.recognition.stop();
-            }
-        },
         methods: {},
         mounted: function () {
             this.loaded = true;
-            const recognition = new SpeechRecognition();
-            recognition.lang = 'ru-RU';
-            recognition.interimResults = false;
-            recognition.maxAlternatives = 1;
-            recognition.continuous = true;
-            recognition.onresult = (event) => {
-                const last = event.results.length - 1;
-                this.rText += event.results[last][0].transcript;
-                this.$forceUpdate();
-                console.log(this.rText);
-                console.log('Confidence: ' + event.results[0][0].confidence);
-            };
-
-            recognition.onnomatch = function(event) {
-                console.log('I didnt recognise that color.');
-            };
-
-            recognition.onerror = function(event) {
-                console.log('Error occurred in recognition: ' + event.error);
-            };
-
-            recognition.onaudioend = function() {
-                console.log('stopped');
-            };
-
-            recognition.start();
-            this.recognition = recognition;
+            enableListener = true;
+            startRecognition(recognition, this);
+        },
+        destroyed: function () {
+            enableListener = false;
+            stopRecognition(recognition);
         }
     });
     return res;
